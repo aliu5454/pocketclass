ab63cada5fe0cc5939a7e8f9b339783c931812ab
Andrew Liu <aliu5454@gmail.com>
2022-09-03 19:04:02 -0400

    search filter and infocard page

--- components/Banner.js
+++ components/Banner.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import Image from 'next/image'
+// import Router, { useRouter } from 'next/router';
 
 function Banner() {
+  // const router = useRouter();
+  // const returnAllClasses = () => {
+  //   router.push({
+  //     pathname: '/search',
+  //     query:{
+  //         location: searchInput,
+  //         startDate: startDate.toISOString(),
+  //         endDate: endDate.toISOString(),
+  //         noOfGuests,
+  //     }
+  // });
+  // }
   return (
     <div className='relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]'>
         <Image src="https://links.papareact.com/0fm" 
